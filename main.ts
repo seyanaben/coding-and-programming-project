38162aed5703caf5cd978574420e852c5c1f7dd3
seyanaben <seyanabenyedder@gmail.com>
2025-11-25 11:39:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
 scene.setBackgroundColor(14)
 game.splash("Welcome To Our Game")
 let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
     . . . . . . f f f f . . . . . . 
-    . . . . f f f e e f f f . . . . 
-    . . . f f f e e e e f f f . . . 
-    . . f f f d d d d d d f f f . . 
-    . . f f d e e e e e e d d f . . 
-    . . f d e f f f f f f e d f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f d d f b f e f f . 
-    . f e e d 1 f d d f 1 d e e f . 
-    . . f e e d d 2 2 d d e e f . . 
-    . . . f e e d d d d e e f . . . 
-    . . e 4 f e e e e e e f 4 e . . 
-    . . 4 d f e e e e e e f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
+    . . . . . f f b b f f . . . . . 
+    . . . . f f b c b b f f . . . . 
+    . . . f f b c b b b b f f . . . 
+    . . . f b b b b b b b b f . . . 
+    . . . f b b f f f f b b f . . . 
+    . . . f b f 9 9 9 9 f b f . . . 
+    . . f f b f 9 9 9 9 f b f f . . 
+    . . f b b b f f f f b b b f . . 
+    . . f b b b b b b b c c b f . . 
+    . . f f f f f f f f f f f f . . 
+    . . . 2 4 5 5 5 5 5 5 4 2 . . . 
+    . . . 2 4 5 5 5 5 5 5 4 2 . . . 
+    . . 2 2 4 5 5 5 5 5 5 4 2 2 . . 
+    . . 2 4 4 5 5 5 5 5 5 4 4 2 . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
